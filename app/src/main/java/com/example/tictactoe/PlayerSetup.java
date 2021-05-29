9d3d52b83db6f57987544ce8d5a9066a193bb5b5
Kalina <salvatorekalina@gmail.com>
2021-05-29 16:52:44 +0300

    Added Google Sign-in icon.

--- app/src/main/java/com/example/tictactoe/PlayerSetup.java
+++ app/src/main/java/com/example/tictactoe/PlayerSetup.java
@@ -4,13 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class PlayerSetup extends AppCompatActivity {
 
     private EditText player1;
     private EditText player2;
+
+    SignInButton googleLogin;
+    public GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,21 @@ public class PlayerSetup extends AppCompatActivity {
 
         player1 = findViewById(R.id.player1Name);
         player2 = findViewById(R.id.player2Name);
+
+        googleLogin = findViewById(R.id.sign_in_google);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(PlayerSetup.this, gso);
+
+        googleLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
     }
 
     public void submitButtonClick(View view) {
@@ -28,5 +57,36 @@ public class PlayerSetup extends AppCompatActivity {
         Intent intent = new Intent(this, GameDisplay.class);
         intent.putExtra("PLAYER_NAMES", new String[]{player1Name, player2Name});
         startActivity(intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+
+            Toast.makeText(this, "Sign In Successfully! "+ account.getEmail(), Toast.LENGTH_SHORT).show();
+            String name=account.getDisplayName();
+            player1.setText("" + name);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Tag", "signInResult:failed code=" + e.getStatusCode());
+            // updateUI(null);
+            Toast.makeText(this, "Sign In issue with code="+e.getStatusCode(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
